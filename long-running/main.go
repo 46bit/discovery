54a46e06dd87027916fbbaf8b6a2adbca2588c1d
Michael Mokrysz <hi@46b.it>
2017-10-29 19:51:16 +0000

    Run a webserver in long-running.

--- long-running/main.go
+++ long-running/main.go
@@ -3,12 +3,22 @@ package main
 import (
   "log"
   "time"
+  "fmt"
+  "net/http"
 )
 
 func main() {
   log.Println("Long running!")
-  for i := 0; i < 3600; i++ {
-    log.Println(i)
-    time.Sleep(time.Second)
-  }
+
+  go func() {
+    for i := 0; i < 3600; i++ {
+      log.Println(i)
+      time.Sleep(time.Second)
+    }
+  }()
+
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+  })
+  http.ListenAndServe(":8080", nil)
 }
