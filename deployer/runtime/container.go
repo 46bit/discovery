26159539b1546e93e6bba06cb062f92a79ed26f9
Michael Mokrysz <hi@46b.it>
2017-11-24 14:03:58 +0000

    Break container operations onto Container type

--- deployer/runtime/container.go
+++ deployer/runtime/container.go
@@ -3,14 +3,16 @@ const (
   Described ContainerState = iota
   // A Container whose containerd.Container has been created
   Created
-  // A Container whose containerd.Task has been created and started
+  // A Container whose containerd.Task has been created
+  Prepared
+  // A Container whose containerd.Task has been started
   Started
   // A Container that has been SIGTERMed
   Sigtermed
   // A Container that has been SIGKILLed
   Sigkilled
   // A Container that has exited
-  Stopped
+  Exited
   // A Container whose containerd.Task has been deleted
   Deleted
   // A Container whose containerd.Container has been deleted
@@ -20,15 +22,97 @@ const (
 type ContainerState uint
 
 // RESTARTING BUT WITH A BRAND-NEW CONTAINER AND TASK:
-//   Created -> Started -> Stopped -> Deleted -> Erased -> Created -> ...
+//   Created -> Prepared -> Started -> Stopped -> Deleted -> Erased -> Created -> ...
 // STOPPING:
-//   Created -> Started -> Sigtermed -> Stopped -> Deleted -> Erased
+//   Created -> Prepared -> Started -> Sigtermed -> Stopped -> Deleted -> Erased
 // FORCE-STOPPING AFTER TIMEOUT:
-//   Created -> Started -> Sigtermed -> Sigkilled -> Stopped -> Deleted -> Erased
+//   Created -> Prepared -> Started -> Sigtermed -> Sigkilled -> Stopped -> Deleted -> Erased
 
 type Container struct {
   ID        string
   Remote    string
   Namespace string
   State     ContainerState
+}
+
+func (c *Container) Create(client *containerd.Client, ctx *context.Context) (*containerd.Container, error) {
+  containerdContainer, err := createContainer(client, ctx, c.ID, c.Remote)
+  if err != nil {
+    return nil, err
+  }
+  c.State = Created
+  return containerdContainer, nil
+}
+
+func (c *Container) Prepare(ctx *context.Context, containerdContainer containerd.Container) (*containerd.Task, exitStatusC <-chan containerd.ExitStatus, error) {
+  containerdTask, err := createTask(ctx, containerdContainer)
+  if err != nil {
+    return nil, err
+  }
+  c.State = Prepared
+  return containerdTask, nil
+}
+
+func (c *Container) Start(ctx *context.Context, containerdTask containerd.Task) (exitStatusC <-chan containerd.ExitStatus, error) {
+  exitStatusC, err := startTask(ctx, containerdTask)
+  if err != nil {
+    return nil, nil, err
+  }
+  c.State = Prepared
+  return exitStatusC, nil
+}
+
+func (c *Container) Sigterm(ctx *context.Context, containerdTask containerd.Task) error {
+  err := c.signal(ctx, containerdTask, syscall.SIGTERM)
+  if err != nil {
+    return err
+  }
+  c.State = Sigtermed
+  return nil
+}
+
+func (c *Container) Sigkill(ctx *context.Context, containerdTask containerd.Task) error {
+  err := c.signal(ctx, containerdTask, syscall.SIGKILL)
+  if err != nil {
+    return err
+  }
+  c.State = Sigkilled
+  return nil
+}
+
+func (c *Container) signal(ctx *context.Context, containerdTask containerd.Task, signal syscall.Signal) error {
+  status, err := containerdTask.Status(ctx)
+  if err != nil {
+    return err
+  }
+  switch status.Status {
+  case containerd.Running:
+    fallthrough
+  case containerd.Paused:
+    fallthrough
+  case containerd.Pausing:
+    err = containerdTask.Kill(ctx, signal, containerd.WithKillAll)
+    if err != nil {
+      return err
+    }
+  }
+  return nil
+}
+
+func (c *Container) Delete(ctx *context.Context, containerdTask containerd.Task) error {
+  _, err := containerdTask.Delete(ctx)
+  if err != nil {
+    return fmt.Errorf("Error deleting task %s: %s", id, err)
+  }
+  c.State = Deleted
+  return nil
+}
+
+func (c *Container) Erase(client *containerd.Client, ctx *context.Context, containerdContainer containerd.Container) error {
+  err = containerdContainer.Delete(ctx, containerd.WithSnapshotCleanup)
+  if err != nil {
+    return err
+  }
+  c.State = Erased
+  return nil
 }
