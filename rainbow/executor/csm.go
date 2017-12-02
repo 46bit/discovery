edd4fe49bca76f8b967ad9079e599d84d8075e49
Michael Mokrysz <hi@46b.it>
2017-12-02 00:15:17 +0000

    Remove duplicate state setting

--- rainbow/executor/csm.go
+++ rainbow/executor/csm.go
@@ -12,7 +12,6 @@ func transition(api cdApi, container *container) error {
 			if err := container.task.start(api); err != nil {
 				return err
 			}
-			container.task.state = started
 		} else if container.task.state == stopped {
 			if err := container.task.delete(api); err != nil {
 				return err
